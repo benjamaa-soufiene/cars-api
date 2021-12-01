03491c7764d97a58390742e12de0ffce3bfc687f
ben jamaa soufiene <gcr.soufiene@gmail.com>
2021-12-01 15:51:32 +0000

    Update CarsApplication.java

--- src/main/java/com/truckla/cars/CarsApplication.java
+++ src/main/java/com/truckla/cars/CarsApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CarsApplication {
 
 	public static void main(String[] args) {
+		int x=20;
 		SpringApplication.run(CarsApplication.class, args);
 	}
 
